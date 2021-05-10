678e15dca89c0beb9b8e0b834106fde116c6be98
2DGraphics-someMaths <cristiano900520@gmail.com>
2021-05-10 12:01:24 +0200

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace In_Lec
+{
+    class Circle
+    {
+        public double XC, YC, Rad, OriginalXC, OriginalYC;
+        public double sTh, eTh;
+        public double MY, MX;
+        public double cx, cy;
+        public int dir;
+        public bool x_based = true, drawL = false;
+        public int spd = 10;
+        public double dx, dy, m, inv_m;
+        
+        public void OriginalCalc()
+        {
+            OriginalXC = XC;
+            OriginalYC = YC;
+        }
+
+        public void DrawPart(Graphics g)
+        {
+            
+            Pen P = new Pen(Color.SkyBlue , 2);
+            for (double cTheta = sTh; cTheta < eTh; cTheta++)
+            {
+                
+
+                cx = Rad * Math.Cos(cTheta * Math.PI / 180) + XC;
+                cy = Rad * Math.Sin(cTheta * Math.PI / 180) + YC;
+
+                if (cTheta == sTh || cTheta == eTh-1)   // drawing the 2 lines of the segment
+                    g.DrawLine(P, (int)cx, (int)cy, (int)XC, (int) YC);
+
+                if (cTheta == (sTh + eTh-1) / 2)    // getting the mid points of the segment.
+                {
+                    MX = Rad * Math.Cos(cTheta * Math.PI / 180) + XC;
+                    MY = Rad * Math.Sin(cTheta * Math.PI / 180) + YC;
+                }
+                //g.DrawLine(Brushes.White, (int) MX)
+                g.FillEllipse(Brushes.Yellow, (int)cx - 3, (int)cy - 3, 6, 6);
+                //if (drawL)
+                    
+            }
+        }
+
+        public void Calc()
+        {
+            
+            dy = MY - YC;
+            dx = MX - XC;
+            m = dy / dx;
+            cx = XC;
+            cy = YC;
+            drawL = true;
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                x_based = true;
+                if (XC < MX)
+                    dir = 1;
+                else
+                    dir = -1;
+            }
+            else
+            {
+                x_based = false;
+                if (YC < MY)
+                    dir = 1;
+                else
+                    dir = -1;
+            }
+        }
+
+        public void MoveStep()
+        {
+            if (x_based)
+            {
+                XC += dir * spd;
+                YC += dir * m * spd;
+            }
+            else
+            {
+                YC += dir * spd;
+                XC += dir * (1.0f / m) * spd;
+            }
+            
+
+        }
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace In_Lec
+{
+    public partial class Form1 : Form
+    {
+        Bitmap off;
+        Timer tt = new Timer();
+        Circle cr = new Circle();
+        List<Circle> L = new List<Circle>();
+        int Selector = 0;
+
+        public Form1()
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.Load += new EventHandler(Form1_Load);
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            tt.Tick += new EventHandler(tt_Tick);
+                    
+        }
+
+        void tt_Tick(object sender, EventArgs e)
+        {
+
+            L[Selector].Calc();
+            
+            L[Selector].MoveStep();
+            
+            DrawDubb(this.CreateGraphics());
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    tt.Start();
+                    break;
+
+                case Keys.Up:
+                    if (Selector < 7)
+                        Selector++;
+                    else
+                        Selector = 0;
+                    break;
+
+                case Keys.Down:
+                    if (Selector > 0)
+                        Selector--;
+                    else
+                        Selector = 7;
+                    break;
+            }
+            DrawDubb(this.CreateGraphics());
+
+        }
+
+        void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DrawDubb(this.CreateGraphics());
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDubb(e.Graphics);    
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            off = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            cr.sTh = 0;
+            cr.eTh = 360;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Circle pnn = new Circle();
+                pnn.XC = 1000;
+                pnn.YC = 500;
+                pnn.cx = pnn.XC;
+                pnn.cy = pnn.YC;
+                pnn.Rad = 200;
+                pnn.sTh = 45 * i;
+                pnn.eTh = pnn.sTh + 45;
+                pnn.OriginalCalc();
+                pnn.drawL = true;
+                L.Add(pnn);
+            }
+        }
+
+        void DrawScene(Graphics g)
+        {
+            g.Clear(Color.Black);
+
+            for (int i = 0; i < L.Count; i++)
+            {
+                L[i].DrawPart(g);
+            }
+            if (L[Selector].drawL)
+            g.DrawLine(Pens.Red, (int)L[Selector].OriginalXC, (int)L[Selector].OriginalYC, (int)L[Selector].MX, (int)L[Selector].MY);
+
+        }
+
+        void DrawDubb(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
+        }
+    }
+}
